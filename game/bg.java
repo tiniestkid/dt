c746e9e306847e5c4cffc87d35c626e7b967285f
tiniestkid <m2000eng426@gmail.com>
2018-06-15 15:20:50 -0700

    updated

--- game/bg.java
+++ game/bg.java
@@ -2,13 +2,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.awt.Dimension;
 import java.awt.Toolkit;
 public class bg extends World{
-public bg(){
-        super((int)(Toolkit.getDefaultToolkit().getScreenSize().getWidth()*0.8),
-              (int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight()*0.8),1);
-              
-      //Debug
-      System.out.println("Debug: World Dimensiions = (" + getWidth()+","+ getHeight()+")");
-                     
-}
-}
-
+    private int screenWidth = getWidth();
+    private int screenHeight = getHeight();
+    public bg(){
+            super((int)(Toolkit.getDefaultToolkit().getScreenSize().getWidth()*0.8),
+                  (int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight()*0.8),1);
+                
+          addObject(new person(), (int)(screenWidth*0.7),(int)(screenHeight*0.5));
+           //Debug
+          System.out.println("Debug: World Dimensiions = (" + getWidth()+","+ getHeight()+")");
+        }
+    }
